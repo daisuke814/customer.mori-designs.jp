da507b298b21111a56c4b01be27cf643fdb57220
Daisuke Sakamoto <41839638+daisuke814@users.noreply.github.com>
2020-05-16 20:54:57 +0900

    no message

--- resources/views/setting.blade.php
+++ resources/views/setting.blade.php
@@ -6,7 +6,18 @@
 
         <div class="card-body">
 
-
+            <div class="row">
+                <div class="col-9">
+                    <h4>メールの受信</h4>
+                    <hr class="mt-0 mb-1">
+                    <p>メッセージが送信されると同時にお客様へメールを送信します。</p>
+                </div>
+                <div class="col-3">
+                    <form action="" method="post">
+                        <input type="checkbox" name="" id="" class="form-control">
+                    </form>
+                </div>
+            </div>
 
         </div>
     </div>
